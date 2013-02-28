81f4eb131e3c2266b57c7c224a07cec15999b25b
Tim Levett <levett@wisc.edu>
2013-02-28 11:11:17 -0600

    [WCP-3] Initial configuration.

---
+++ src/main/java/org/jasig/portlet/blackboardvcportlet/service/UserService.java
@@ -0,0 +1,9 @@
+package org.jasig.portlet.blackboardvcportlet.service;
+
+import org.jasig.portlet.blackboardvcportlet.data.User;
+
+public interface UserService {
+	
+	public User getUserDetails(String searchTerm);
+	
+}
