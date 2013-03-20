33d78ffe7ebf5e4066c9ca2093ae34f62d107428
Brad Leege <bleege@gmail.com>
2013-03-20 17:12:41 -0500

    [WCP-29] Refactoring SAS Web Service security credentialing into a custom WebServiceTemplate and adding more assertions to Integration Test.

---
+++ src/main/java/org/jasig/portlet/blackboardvcportlet/service/util/SASWebServiceTemplate.java
@@ -0,0 +1,58 @@
+/**
+ * @author Brad Leege <leege@doit.wisc.edu>
+ * Created on 3/20/13 at 4:49 PM
+ */
+package org.jasig.portlet.blackboardvcportlet.service.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.WebServiceMessage;
+import org.springframework.ws.client.core.WebServiceMessageCallback;
+import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.soap.saaj.SaajSoapMessage;
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPElement;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+
+public class SASWebServiceTemplate extends WebServiceTemplate
+{
+	@Autowired
+	private Jaxb2Marshaller elluminateMarshller;
+
+	/**
+	 * Default Constructor
+	 */
+	public SASWebServiceTemplate()
+	{
+		super();
+	}
+
+	public Object marshalSendAndReceive(final String soapAction, final Object requestPayload)
+	{
+		return marshalSendAndReceive(requestPayload, new WebServiceMessageCallback() {
+
+			@Override
+			public void doWithMessage(WebServiceMessage webServiceMessage) throws IOException, TransformerException
+			{
+				if (webServiceMessage instanceof SaajSoapMessage)
+				{
+					final SaajSoapMessage casted = (SaajSoapMessage) webServiceMessage;
+					casted.setSoapAction(soapAction);
+
+					QName qName = new QName(elluminateMarshller.getContextPath(), "BasicAuth", "sas");
+					try
+					{
+						SOAPElement baHeader = casted.getSaajMessage().getSOAPHeader().addChildElement(qName);
+						baHeader.addChildElement(new QName("sas", "Name")).addTextNode("WISCONSIN-ADMIN");
+						baHeader.addChildElement(new QName("sas", "Password")).addTextNode("Wared4aM");
+					}
+					catch (Exception e)
+					{
+						logger.error("Error creating SOAPHeader: ", e);
+					}
+				}
+			}
+		});
+	}
+}
--- src/test/java/org/jasig/portlet/blackboardvcportlet/test/WebServiceIntegrationTest.java
+++ src/test/java/org/jasig/portlet/blackboardvcportlet/test/WebServiceIntegrationTest.java
@@ -7,24 +7,17 @@ package org.jasig.portlet.blackboardvcportlet.test;
 import com.elluminate.sas.GetServerQuotasResponseCollection;
 import com.elluminate.sas.ObjectFactory;
 import com.elluminate.sas.ServerQuotasResponse;
+import org.jasig.portlet.blackboardvcportlet.service.util.SASWebServiceTemplate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.ws.WebServiceMessage;
-import org.springframework.ws.client.core.WebServiceMessageCallback;
-import org.springframework.ws.client.core.WebServiceTemplate;
-import org.springframework.ws.soap.saaj.SaajSoapMessage;
-import javax.xml.namespace.QName;
-import javax.xml.soap.SOAPElement;
-import javax.xml.transform.TransformerException;
-import java.io.IOException;
 import java.util.List;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:/test-applicationContext.xml")
@@ -33,10 +26,7 @@ public class WebServiceIntegrationTest
 	private static final Logger logger = LoggerFactory.getLogger(WebServiceIntegrationTest.class);
 
 	@Autowired
-	private WebServiceTemplate webServicesTemplate;
-
-	@Autowired
-	private Jaxb2Marshaller elluminateMarshller;
+	private SASWebServiceTemplate sasWebServiceTemplate;
 
 	private static final ObjectFactory OBJECT_FACTORY = new ObjectFactory();
 
@@ -44,36 +34,16 @@ public class WebServiceIntegrationTest
 	public void testConnection() throws Exception
 	{
 		logger.info("Starting testConnection()...");
-
-		GetServerQuotasResponseCollection serverQuotasResponseCollection = (GetServerQuotasResponseCollection)webServicesTemplate.marshalSendAndReceive(OBJECT_FACTORY.createGetServerQuotas(null), new WebServiceMessageCallback() {
-
-			@Override
-			public void doWithMessage(WebServiceMessage webServiceMessage) throws IOException, TransformerException
-			{
-				if (webServiceMessage instanceof SaajSoapMessage)
-				{
-					final SaajSoapMessage casted = (SaajSoapMessage) webServiceMessage;
-					casted.setSoapAction("http://sas.elluminate.com/GetServerQuotas");
-
-					QName qName = new QName(elluminateMarshller.getContextPath(), "BasicAuth", "sas");
-					try
-					{
-						SOAPElement baHeader = casted.getSaajMessage().getSOAPHeader().addChildElement(qName);
-						baHeader.addChildElement(new QName("sas", "Name")).addTextNode("WISCONSIN-ADMIN");
-						baHeader.addChildElement(new QName("sas", "Password")).addTextNode("Wared4aM");
-					}
-					catch (Exception e)
-					{
-						 logger.error("Error creating SOAPHeader: ", e);
-					}
-				}
-			}
-		});
-
+		GetServerQuotasResponseCollection serverQuotasResponseCollection = (GetServerQuotasResponseCollection)sasWebServiceTemplate.marshalSendAndReceive("", OBJECT_FACTORY.createGetServerQuotas(null));
 		assertNotNull(serverQuotasResponseCollection);
 		List<ServerQuotasResponse> quotaResult = serverQuotasResponseCollection.getServerQuotasResponses();
 		assertNotNull(quotaResult);
 		assertTrue(quotaResult.size() > 0);
+		ServerQuotasResponse response = quotaResult.get(0);
+		assertNotNull(response.getDiskQuota());
+		assertNotNull(response.getDiskQuotaAvailable());
+		assertNotNull(response.getSessionQuota());
+		assertNotNull(response.getSessionQuotaAvailable());
 		logger.info("Finished testConnection().");
 	}
 }
