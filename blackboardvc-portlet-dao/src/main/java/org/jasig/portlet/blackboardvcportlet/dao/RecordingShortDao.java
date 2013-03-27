11ed17b2824d960647e573c726f083d62569eb12
Brad Leege <bleege@gmail.com>
2013-03-27 14:48:39 -0500

    [WCP-48] Breaking project into blackboard-portlet-api, blackboard-portlet-dao, and blackboard-portlet-webapp modules

---
+++ blackboardvc-portlet-dao/src/main/java/org/jasig/portlet/blackboardvcportlet/dao/RecordingShortDao.java
@@ -0,0 +1,44 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jasig.portlet.blackboardvcportlet.dao;
+
+import org.jasig.portlet.blackboardvcportlet.data.RecordingShort;
+import java.util.List;
+
+/**
+ * DAO Interface class for the RecordingShort
+ * @author Richard Good
+ */
+public interface RecordingShortDao {
+    
+    public RecordingShort getRecording(long recordingShortId);
+    
+    public void deleteRecordingShort(long recordingShortId);
+    
+    public void saveRecordingShort(RecordingShort recordingShort);
+    
+    public void deleteAllRecordingShort(long sessionId);
+    
+    public List<RecordingShort> getAllRecordings();
+    
+    public List<RecordingShort> getRecordingsForUser(String uid);
+    
+    public List<RecordingShort> getAllSessionRecordings(long sessionId);
+    
+}
---
+++ blackboardvc-portlet-dao/src/main/java/org/jasig/portlet/blackboardvcportlet/data/RecordingShort.java
@@ -0,0 +1,155 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jasig.portlet.blackboardvcportlet.data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Entity class for storing recordings.
+ * @author Richard Good
+ */
+@Entity
+@Table(name="VC_RECORDING_SHORT")
+public class RecordingShort implements Serializable{
+    
+    @Id
+    @Column(name="RECORDING_ID")
+    protected long recordingId;
+    
+    @Column(name="CREATION_DATE")
+    protected long creationDate;
+    
+    @Column(name="RECORDING_SIZE")
+    protected long recordingSize;
+    
+    @Column(name="ROOM_NAME")
+    protected String roomName;
+    
+    @Column(name="SESSION_ID")
+    protected long sessionId;
+    
+    @Column(name="CHAIR_LIST")
+    protected String chairList;
+    
+    @Column(name="NON_CHAIR_LIST")
+    protected String nonChairList;
+    
+    @Transient
+    protected String recordingUrl;
+    
+    @Transient
+    protected boolean currUserCanDelete;
+    
+    @Transient
+    protected Date createdDate;
+    
+    @Transient 
+    protected String readableFileSize;
+
+    public String getReadableFileSize() {
+        return readableFileSize;
+    }
+
+    public void setReadableFileSize(String readableFileSize) {
+        this.readableFileSize = readableFileSize;
+    }
+
+    public String getChairList() {
+        return chairList;
+    }
+
+    public void setChairList(String chairList) {
+        this.chairList = chairList;
+    }
+
+    public String getNonChairList() {
+        return nonChairList;
+    }
+
+    public void setNonChairList(String nonChairList) {
+        this.nonChairList = nonChairList;
+    }
+  
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+    
+    public boolean isCurrUserCanDelete() {
+        return currUserCanDelete;
+    }
+
+    public void setCurrUserCanDelete(boolean currUserCanDelete) {
+        this.currUserCanDelete = currUserCanDelete;
+    }
+    
+    public String getRecordingUrl() {
+        return recordingUrl;
+    }
+
+    public void setRecordingUrl(String recordingUrl) {
+        this.recordingUrl = recordingUrl;
+    }
+    
+    public long getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(long creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public long getRecordingId() {
+        return recordingId;
+    }
+
+    public void setRecordingId(long recordingId) {
+        this.recordingId = recordingId;
+    }
+
+    public long getRecordingSize() {
+        return recordingSize;
+    }
+
+    public void setRecordingSize(long recordingSize) {
+        this.recordingSize = recordingSize;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public long getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(long sessionId) {
+        this.sessionId = sessionId;
+    }
+        
+}
