41c2876046e889986ad4160d8ab934ef37d24f2d
Tim Levett <levett@wisc.edu>
2013-04-09 09:08:06 -0500

    WCP-76 : Updated callback function to throw a 404 error if/when someone uses a invalid security token.

--- blackboardvc-portlet-webapp/src/main/java/org/jasig/portlet/blackboardvcportlet/mvc/callback/BlackboardVCPortletCallbackController.java
+++ blackboardvc-portlet-webapp/src/main/java/org/jasig/portlet/blackboardvcportlet/mvc/callback/BlackboardVCPortletCallbackController.java
@@ -24,10 +24,12 @@ import org.jasig.portlet.blackboardvcportlet.service.ServerConfigurationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.portlet.bind.annotation.ResourceMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -63,6 +65,11 @@ public class BlackboardVCPortletCallbackController implements ServletContextAwar
 	public void setServletContext(ServletContext servletContext) {
 		this.servletContext = servletContext;
 	}
+	
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public class ResourceNotFoundException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+	}
 
 	/**
      * Callback method. Looks for a passed session_id and updates the recordings
@@ -81,6 +88,7 @@ public class BlackboardVCPortletCallbackController implements ServletContextAwar
         	recordingService.updateSessionRecordings(Long.valueOf(sessionId));
         } else {
         	logger.error("Invalid callback URL provided. Expected :" + serverConfiguration.getRandomCallbackUrl() + "; Received : " + securityToken);
+        	throw new ResourceNotFoundException();
         }
         
         return new ModelAndView("callback");
