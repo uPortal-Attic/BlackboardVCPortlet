21df67d7be3bb1e615ef42b3599e055e3ff43894
Tim Levett <levett@wisc.edu>
2013-04-19 10:59:57 -0500

    WCP-85 : fixed unit tests for Global config

--- blackboardvc-portlet-webapp/src/test/java/org/jasig/portlet/blackboardvcportlet/dao/ws/impl/GlobalSettingsWSDaoTest.java
+++ blackboardvc-portlet-webapp/src/test/java/org/jasig/portlet/blackboardvcportlet/dao/ws/impl/GlobalSettingsWSDaoTest.java
@@ -1,8 +1,5 @@
 package org.jasig.portlet.blackboardvcportlet.dao.ws.impl;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
@@ -11,73 +8,91 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
 import org.jasig.portlet.blackboardvcportlet.dao.ws.GlobalSettingsWSDao;
+import org.jasig.portlet.blackboardvcportlet.service.util.SASWebServiceOperations;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.dao.support.DataAccessUtils;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.elluminate.sas.BlackboardGetServerConfigurationResponseCollection;
 import com.elluminate.sas.BlackboardGetServerQuotasResponseCollection;
 import com.elluminate.sas.BlackboardGetServerVersionResponseCollection;
-import com.elluminate.sas.BlackboardServerConfigurationResponse;
-import com.elluminate.sas.BlackboardServerQuotasResponse;
-import com.elluminate.sas.BlackboardServerVersionResponse;
+import com.elluminate.sas.BlackboardSuccessResponse;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class GlobalSettingsWSDaoTest extends GlobalSettingsWSDaoImplIT {
 	
-	@Mock 
+	@Autowired 
 	private GlobalSettingsWSDao dao;
+	
+	@Mock
+	private SASWebServiceOperations sasWebServiceTemplate;
 	
 	@Before
 	public void thebefore() {
+		MockitoAnnotations.initMocks(this);
+		dao.setSasWebServiceTemplate(sasWebServiceTemplate);
 		super.setGlobalSettingWSDao(dao);
 	}
 	
 	@Test
 	public void getServerConfigurationTest() throws Exception  {
-		when(dao.getServerConfiguration()).thenReturn(getServerConfigMock());
+		when(sasWebServiceTemplate.marshalSendAndReceiveToSAS(Matchers.contains("http://sas.elluminate.com/GetServerConfiguration"), any(Object.class))).thenReturn(getServerConfigMock());
 		super.getServerConfigurationTest();
 	}
 	
 	@Test
 	public void getServerQuotaTest() throws Exception {
-		when(dao.getServerQuota()).thenReturn(getServerQuotaMock());
+		when(sasWebServiceTemplate.marshalSendAndReceiveToSAS(Matchers.contains("http://sas.elluminate.com/GetServerQuota"), any(Object.class))).thenReturn(getServerQuotaMock());
 		super.getServerQuotaTest();
 	}
 	
 	@Test
 	public void getServerVersionsTest() throws Exception {
-		when(dao.getServerVersions()).thenReturn(getServerVersionsMock());
+		when(sasWebServiceTemplate.marshalSendAndReceiveToSAS(Matchers.contains("http://sas.elluminate.com/GetServerVersion"), any(Object.class))).thenReturn(getServerVersionsMock());
 		super.getServerVersionsTest();
 	}
 	
+	@SuppressWarnings("rawtypes")
 	@Test
 	public void setApiCallbackUrlTest() throws Exception {
-		when(dao.setApiCallbackUrl(any(String.class))).thenReturn(true);
+		when(sasWebServiceTemplate.marshalSendAndReceiveToSAS(Matchers.contains("http://sas.elluminate.com/SetApiCallbackUrl"), any(Object.class))).thenAnswer(new Answer() {
+
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+				BlackboardSuccessResponse blackboardSuccessResponse = new BlackboardSuccessResponse();
+				blackboardSuccessResponse.setSuccess(true);
+				return blackboardSuccessResponse;
+			}
+			
+		});
 		super.setApiCallbackUrlTest();
 	}
 	
-	private BlackboardServerVersionResponse getServerVersionsMock() throws JAXBException {
+	private BlackboardGetServerVersionResponseCollection getServerVersionsMock() throws JAXBException {
 		final JAXBContext context = JAXBContext.newInstance(BlackboardGetServerVersionResponseCollection.class);
         final Unmarshaller unmarshaller = context.createUnmarshaller();
         final BlackboardGetServerVersionResponseCollection result = (BlackboardGetServerVersionResponseCollection)unmarshaller.unmarshal(this.getClass().getResourceAsStream("/data/serverVersionsResponse.xml"));
-        return DataAccessUtils.singleResult(result.getServerVersionResponses());
+        return result;
 	}
 	
-	private BlackboardServerQuotasResponse getServerQuotaMock() throws JAXBException {
+	private BlackboardGetServerQuotasResponseCollection getServerQuotaMock() throws JAXBException {
 		final JAXBContext context = JAXBContext.newInstance(BlackboardGetServerQuotasResponseCollection.class);
         final Unmarshaller unmarshaller = context.createUnmarshaller();
         final BlackboardGetServerQuotasResponseCollection result = (BlackboardGetServerQuotasResponseCollection)unmarshaller.unmarshal(this.getClass().getResourceAsStream("/data/serverQuotaResponse.xml"));
-        return DataAccessUtils.singleResult(result.getServerQuotasResponses());
+        return result;
 	}
 	
-	private BlackboardServerConfigurationResponse getServerConfigMock() throws JAXBException {
+	private BlackboardGetServerConfigurationResponseCollection getServerConfigMock() throws JAXBException {
 		final JAXBContext context = JAXBContext.newInstance(BlackboardGetServerConfigurationResponseCollection.class);
         final Unmarshaller unmarshaller = context.createUnmarshaller();
         final BlackboardGetServerConfigurationResponseCollection result = (BlackboardGetServerConfigurationResponseCollection)unmarshaller.unmarshal(this.getClass().getResourceAsStream("/data/serverConfigurationResponse.xml"));
-        return DataAccessUtils.singleResult(result.getServerConfigurationResponses());
+        return result;
 	}
 }
