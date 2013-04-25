2d117d061fef0767c3fe2d127c3e4495757b055b
Brad Leege <bleege@gmail.com>
2013-04-25 15:48:10 -0500

    [WCP-146] Adding unit tests for QuarterHourCheck annotation

---
+++ blackboardvc-portlet-api/src/test/java/org/jasig/portlet/blackboardvcportlet/validations/test/CustomValidationsTest.java
@@ -0,0 +1,53 @@
+/**
+ * @author Brad Leege <leege@doit.wisc.edu>
+ * Created on 4/25/13 at 3:31 PM
+ */
+package org.jasig.portlet.blackboardvcportlet.validations.test;
+
+import org.jasig.portlet.blackboardvcportlet.service.SessionForm;
+import org.joda.time.DateMidnight;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+import static org.junit.Assert.assertEquals;
+
+public class CustomValidationsTest
+{
+	private static Logger logger = LoggerFactory.getLogger(CustomValidationsTest.class);
+	private static Validator validator;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		validator = Validation.buildDefaultValidatorFactory().getValidator();
+	}
+
+	/**
+	 * Tests the QuarterHourCheck
+	 * The StartMinute should pass, while the EndMinute should trigger a violation.
+	 */
+	@Test
+	public void testQuarterHourCheck()
+	{
+		logger.info("testQuarterHourCheck() started....");
+		SessionForm sessionForm = new SessionForm();
+		sessionForm.setSessionName("Test Session");
+		sessionForm.setBoundaryTime(15);
+		sessionForm.setStartDate(DateMidnight.now());
+		sessionForm.setStartHour(20);
+		sessionForm.setStartMinute(15);
+		sessionForm.setEndDate(DateMidnight.now());
+		sessionForm.setEndHour(21);
+		sessionForm.setEndMinute(35);
+
+		Set<ConstraintViolation<SessionForm>> constraintViolations = validator.validate(sessionForm);
+		assertEquals(1, constraintViolations.size());
+		assertEquals("endMinute", constraintViolations.iterator().next().getPropertyPath().toString());
+		logger.info("testQuarterHourCheck() finished.");
+	}
+}
