bf09f95fa645f2b0ab9ec09b7f515454b9beef51
Brad Leege <bleege@gmail.com>
2013-04-26 10:06:19 -0500

    [WCP-149] Fixing failing unit test from last night by setting startDate and endDate in sessionForm to 7 days out from runtime so that the @Future annotation won't trip the tests up when run at 8 PM.

--- blackboardvc-portlet-api/src/test/java/org/jasig/portlet/blackboardvcportlet/validations/test/CustomValidationsTest.java
+++ blackboardvc-portlet-api/src/test/java/org/jasig/portlet/blackboardvcportlet/validations/test/CustomValidationsTest.java
@@ -7,7 +7,6 @@ package org.jasig.portlet.blackboardvcportlet.validations.test;
 import org.jasig.portlet.blackboardvcportlet.service.SessionForm;
 import org.joda.time.DateMidnight;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,18 +32,17 @@ public class CustomValidationsTest
 	 * The StartMinute should pass, while the EndMinute should trigger a violation.
 	 */
 	@Test
-	@Ignore
 	public void testQuarterHourCheck()
 	{
 		logger.info("testQuarterHourCheck() started....");
 		SessionForm sessionForm = new SessionForm();
 		sessionForm.setSessionName("Test Session");
 		sessionForm.setBoundaryTime(15);
-		sessionForm.setStartDate(DateMidnight.now());
-		sessionForm.setStartHour(20);
+		sessionForm.setStartDate(DateMidnight.now().plusDays(7));
+		sessionForm.setStartHour(5);
 		sessionForm.setStartMinute(15);
-		sessionForm.setEndDate(DateMidnight.now());
-		sessionForm.setEndHour(21);
+		sessionForm.setEndDate(sessionForm.getStartDate());
+		sessionForm.setEndHour(6);
 		sessionForm.setEndMinute(35);
 
 		Set<ConstraintViolation<SessionForm>> constraintViolations = validator.validate(sessionForm);
@@ -60,17 +58,16 @@ public class CustomValidationsTest
 	 * @throws Exception
 	 */
 	@Test
-	@Ignore
 	public void testSessionEndTimeRangeCheck() throws Exception
 	{
 		logger.info("testSessionEndTimeRangeCheck() started....");
 		SessionForm sessionForm = new SessionForm();
 		sessionForm.setSessionName("Test Session");
 		sessionForm.setBoundaryTime(15);
-		sessionForm.setStartDate(DateMidnight.now());
+		sessionForm.setStartDate(DateMidnight.now().plusDays(7));
 		sessionForm.setStartHour(20);
 		sessionForm.setStartMinute(15);
-		sessionForm.setEndDate(DateMidnight.now());
+		sessionForm.setEndDate(sessionForm.getStartDate());
 		sessionForm.setEndHour(21);
 		sessionForm.setEndMinute(45);
 
